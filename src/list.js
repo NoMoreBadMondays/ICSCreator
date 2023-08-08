1bde9b21cd101d59838255547ee9d3475f70059b
NoMoreBadMondays <141739219+NoMoreBadMondays@users.noreply.github.com>
2023-08-08 13:36:55 +0100

    Initial commit

---
+++ src/list.js
@@ -0,0 +1,9 @@
+const items = [
+  {
+    title: "Learning Time",
+    description:
+      "Taking some time out today to learn something new with No More Bad Mondays."
+  }
+];
+
+export default items;
